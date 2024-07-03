1d66154ee8a90a6831b0e7fb68935b7e93faed37
miertink <miertink.vix@hotmail.com>
2024-07-03 16:46:12 -0300

    final version beta 0.1

--- run_old.py
+++ run_old.py
@@ -1,59 +1,69 @@
-#!/bin/python
-
-from zwift import Client
-import paho.mqtt.client as mqtt
 import time
-# import json
+import threading
+import colorsys
 
-# i know... this is not elegant!
-from settings import *
+# Function to convert hex color to RGB tuple
+def hex_to_rgb(hex_color):
+    hex_color = hex_color.lstrip('#')
+    return tuple(int(hex_color[i:i+2], 16) for i in (0, 2, 4))
 
+# Function to convert RGB tuple to hex color
+def rgb_to_hex(rgb_color):
+    return '#{:02x}{:02x}{:02x}'.format(*rgb_color)
 
-#OFFLINE_MSG = json.dumps({'is_online': 0, 'hr': 0, 'power': 0, 'speed': 0.0})
+# Function to interpolate between two RGB colors
+def interpolate_color(start_rgb, end_rgb, steps):
+    return [
+        (
+            int(start_rgb[0] + (end_rgb[0] - start_rgb[0]) * t / steps),
+            int(start_rgb[1] + (end_rgb[1] - start_rgb[1]) * t / steps),
+            int(start_rgb[2] + (end_rgb[2] - start_rgb[2]) * t / steps)
+        )
+        for t in range(steps + 1)
+    ]
 
-if __name__ == "__main__":
+# Function to handle the smooth transition of led_color_hex
+def smooth_transition(start_color, end_color, duration, steps=100):
+    start_rgb = hex_to_rgb(start_color)
+    end_rgb = hex_to_rgb(end_color)
+    interpolated_colors = interpolate_color(start_rgb, end_rgb, steps)
 
-    # mqtt_client = mqtt.Client(mqtt_client_name)
-    # mqtt_client.username_pw_set(mqtt_login, mqtt_pw)
-    # mqtt_client.will_set(mqtt_topic_will, payload="Offline", retain=True)
-    # mqtt_client.connect(mqtt_host_name)
-    # mqtt_client.publish(mqtt_topic_will, payload="Online", retain=True)
-    # mqtt_client.publish(mqtt_topic, "Starting")
+    for color in interpolated_colors:
+        led_color_hex = rgb_to_hex(color)
+        publish_status(mqtt_client, MQTT_BASE_COLOR_TOPIC, led_color_hex)
+        time.sleep(duration / steps)
 
-    client = Client(username, password)
-    world = client.get_world(1)
-#   players(world.players)
-    print('trying to find ' + str(PLAYER_ID))
-
-while (True):
-    error = 0
-    online = False
-    # mqtt_client.loop_start()
-    while (not online):
+# Thread target function
+def led_transition_thread():
+    while online:
         try:
             status = world.player_status(PLAYER_ID)
-            error = 0
-            online = True
-            print(str(PLAYER_ID) + ' appears to be online, check if cycling or running')
+            if status.sport == 0:
+                ring_buffer.add(status.power)
+                power_avg = int(np.mean(ring_buffer.get()))
+                led_color = convert_to_rgb(0, user_power_zone7, power_avg)
+                led_color_hex = ''.join(f'{c:02x}' for c in led_color)
+                msg_dict = {'is_online': 1,
+                            'sport': 'cycling',
+                            'hr': status.heartrate,
+                            'power': status.power,
+                            'power average': power_avg,
+                            'speed': float("{:.2f}".format(float(status.speed) / 1000000.0))}
+                logger.info(msg_dict)
+                publish_status(mqtt_client, MQTT_BASE_COLOR_TOPIC, led_color_hex)
+                publish_status(mqtt_client, MQTT_INFO_TOPIC, payload=json.dumps(msg_dict))
+
+                # Start a new thread for the smooth transition
+                threading.Thread(target=smooth_transition, args=(current_color, led_color_hex, 1)).start()
+                current_color = led_color_hex
+
         except:
-            error += 1
             online = False
-            print(str(PLAYER_ID)+' appears to be offline or error while retrieving player status - trying..' + str(error))
-            time.sleep(5)
-            # mqtt_client.publish("cmnd/miertink_e20fc4/power", 0, retain=True)
-        while (online):
-            try:
-                status = world.player_status(PLAYER_ID)
-                if status.sport == 0:
-                    msg_dict = {'is_online': 1, 'sport': 'cycling', 'hr': status.heartrate, 'power': status.power,
-                                'speed': float("{:.2f}".format(float(status.speed) / 1000000.0))}
-                elif status.sport == 1:
-                    msg_dict = {'is_online': 1, 'sport': 'running', 'hr': status.heartrate,
-                                'speed': float("{:.2f}".format(float(status.speed) / 1000000.0))}
-                #mqtt_client.publish(mqtt_topic, payload=json.dumps(msg_dict), retain=False)
-                print(msg_dict)
-                # mqtt_client.publish("cmnd/miertink_e20fc4/power", 1, retain=False)
-                time.sleep(2)
-            except:
-                online = False
-    # mqtt_client.loop_stop()
+            logger.info(f'{PLAYER_ID} appears to have gone offline')
+        time.sleep(MQTT_ZWIFT_REQUEST_INTERNAL)
+
+# Initialize current color
+current_color = '#000000'
+
+# Start the LED transition thread
+threading.Thread(target=led_transition_thread).start()
